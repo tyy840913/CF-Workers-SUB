fcad1d264bf2a74ec091a5e84824d5d26007d682
tyy840913 <44389998+tyy840913@users.noreply.github.com>
2025-12-02 17:48:08 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -17,7 +17,7 @@ https://cfxr.eu.org/getSub
 `;
 
 let urls = [];
-let subConverter = "SUBAPI.cmliussss.net"; //在线订阅转换后端，目前使用CM的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
+let subConverter = ""; //在线订阅转换后端，目前使用CM的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
 let subConfig = "https://raw.githubusercontent.com/tyy840913/backup/refs/heads/main/ACL4SSR_Online_Full.yaml"; //订阅配置文件
 let subProtocol = 'https';
 
@@ -827,3 +827,4 @@ async function KV(request, env, txt = 'ADD.txt', guest) {
 	}
 
 }
+
